b01650d1249c29f682cf80d04fbefecb47134179
Thien Le <thien@formsquare.co>
2023-09-25 22:15:22 +0700

    Update model_balance_type

--- model_balance_type.go
+++ model_balance_type.go
@@ -1,7 +1,7 @@
 /*
 Synctera API
 
-<h2>Let's build something great.</h2><p>Welcome to the official reference documentation for Synctera APIs. Our APIs are the best way to automate your company's banking needs and are designed to be easy to understand and implement.</p><p>We're continuously growing this library and what you see here is just the start, but if you need something specific or have a question, <a class='text-blue-600' href='https://synctera.com/contact' target='_blank' rel='noreferrer'>contact us</a>.</p> 
+<h2>Let's build something great.</h2><p>Welcome to the official reference documentation for Synctera APIs. Our APIs are the best way to automate your company's banking needs and are designed to be easy to understand and implement.</p><p>We're continuously growing this library and what you see here is just the start, but if you need something specific or have a question, <a class='text-blue-600' href='https://synctera.com/contact' target='_blank' rel='noreferrer'>contact us</a>.</p>
 
 API version: 0.32.0
 */
@@ -15,19 +15,21 @@ import (
 	"fmt"
 )
 
-// BalanceType For `SAVING` and `CHECKING` accounts: * `ACCOUNT_BALANCE`: the amount of money in the account. Equal to the sum of credits minus debits for all posted transactions. * `AVAILABLE_BALANCE`: the account balance minus any pending debits.  For `LINE_OF_CREDIT` accounts: * `ACCOUNT_BALANCE`: the amount of credit currently in use. Equal to the sum of debits minus credits for all posted transactions. * `AVAILABLE_BALANCE`: the amount of credit available. Equal to the credit limit minus `account_balance` minus any pending debits. 
+// BalanceType For `SAVING` and `CHECKING` accounts: * `ACCOUNT_BALANCE`: the amount of money in the account. Equal to the sum of credits minus debits for all posted transactions. * `AVAILABLE_BALANCE`: the account balance minus any pending debits.  For `LINE_OF_CREDIT` accounts: * `ACCOUNT_BALANCE`: the amount of credit currently in use. Equal to the sum of debits minus credits for all posted transactions. * `AVAILABLE_BALANCE`: the amount of credit available. Equal to the credit limit minus `account_balance` minus any pending debits.
 type BalanceType string
 
 // List of balance_type
 const (
 	BALANCETYPE_ACCOUNT_BALANCE BalanceType = "ACCOUNT_BALANCE"
 	BALANCETYPE_AVAILABLE_BALANCE BalanceType = "AVAILABLE_BALANCE"
+	BALANCETYPE_ACCRUED_INTEREST_MTD BalanceType = "ACCRUED_INTEREST_MTD"
 )
 
 // All allowed values of BalanceType enum
 var AllowedBalanceTypeEnumValues = []BalanceType{
 	"ACCOUNT_BALANCE",
 	"AVAILABLE_BALANCE",
+	"ACCRUED_INTEREST_MTD",
 }
 
 func (v *BalanceType) UnmarshalJSON(src []byte) error {
@@ -108,4 +110,3 @@ func (v *NullableBalanceType) UnmarshalJSON(src []byte) error {
 	v.isSet = true
 	return json.Unmarshal(src, &v.value)
 }
-
